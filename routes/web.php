36fb60b2c4bd0ce1ae94ab7060a1241ce6984162
Miyu2484 <a19530104@mmc.atomi.ac.jp>
2021-03-26 20:01:55 +0900

    laravel_basic

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +12,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts', 'PostController@index');
