ce97c89ceecfa13d4941d69132297de6d5b1aeb9
Miyu2484 <a19530104@mmc.atomi.ac.jp>
2021-04-08 13:26:05 +0900

    保存

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,33 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
-
-use App\Category;
 use App\Post;
-use App\Http\Requests\PostRequest;
-=======
-use App\Post;
-
 use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
 
   public function index(Post $post)
-  {
-    return view('index')->with(['posts' => $post->getPaginate]);
+  {//$post = new Post();
+    return view('index')->with(['posts' => $post->getPaginateByLimit()]);
   }
   public function show(Post $post)
   {
     return view('show')->with(['post' => $post]);
   }
-=======
-  dev_basis02
-  public function index(Post $post)
-  {
-    return view('index')->with(['posts' => $post->getPaginateByLimit(1)]);
-  } 
+public function create()
+{
+    return view('create');
+}
 
 
 }
